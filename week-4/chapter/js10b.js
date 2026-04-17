944779fd75f8634715a65cc652763f6ad14b74f0
Leslie Espino <via.esp56@gmail.com>
2026-04-17 11:59:38 -0500

    Completed Hands-On Project 10-1 (Week 4 Drag and Drop API)

---
+++ week-4/chapter/js10b.js
@@ -0,0 +1,18 @@
+"use strict";
+/*  JavaScript 7th Edition
+    Chapter 10
+    Chapter case
+
+    Oak Top House Directions
+    Author: Leslie Espino 
+    Date: April 18th, 2026 
+
+    Filename: js10b.js
+*/
+
+// Function to set up and display the Oak Top House Map
+function initMap() {
+  // Page objects
+  let displayMap = document.getElementById("displayMap");
+  let routeBox = document.getElementById("routeBox");
+}
